94bc68d378dfe3d463ca941098d867420ffef5ae
Jay <w41203208@gmail.com>
2022-09-19 10:18:02 +0800

    remove file from signal server dir which is removing js type file

---
+++ signal-server/src/common/RecordRouter.ts
@@ -0,0 +1,6 @@
+export class RecordRouter {
+  private _id: string;
+  constructor(router_id: string) {
+    this._id = router_id;
+  }
+}
--- signal-server/src/redis/index.js
+++
@@ -1,9 +0,0 @@
-const { createRedisController } = require('./redis.js');
-const { RoomController } = require('./RoomController.js');
-const { SFUServerController } = require('./SFUServerController.js');
-const { PlayerController } = require('./PlayerController.js');
-
-module.exports = {
-  createRedisController: createRedisController,
-  Controllers: [RoomController, SFUServerController, PlayerController],
-};
