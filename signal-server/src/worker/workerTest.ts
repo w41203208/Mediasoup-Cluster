e5e129813d789e0ff0caf2143d674d427dd0f2ce
Jay <w41203208@gmail.com>
2022-10-18 11:59:44 +0800

    new Logger.ts

--- signal-server/src/worker/workerTest.ts
+++
@@ -1,9 +0,0 @@
-import { WorkerPool } from './workerPool';
-
-export const TEST = () => {
-  const workerPool = new WorkerPool({ path: './src/worker/worker.ts', size: 2, strategy: 'roundrobin' });
-
-  workerPool.exec('test', () => {
-    console.log('test');
-  });
-};
